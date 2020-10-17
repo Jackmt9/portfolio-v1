4251eb4445a493eea66df447418b94bfc1d6b9b2
jackmt9 <jackmt9@gmail.com>
2020-10-16 22:02:43 -0400

    components built

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Contact extends React.Component {
+    render() {
+        return(
+            <>
+                contact me here.
+            </>
+        )
+    }
+}
+
+export default Contact
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+    render() {
+        return(
+            <div id="main">
+                <p>Hi there, my name is Jack!</p>
+            </div>
+        )
+    }
+}
+
+export default Main
